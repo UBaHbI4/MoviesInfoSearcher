0aa706534416e24fe282213201f4f94cb83632ee
UBaHbI4 <kinoshnik_ivan@mail.ru>
2021-06-07 23:32:02 +0700

    - Убрал функционал "Переведите хотя бы один экран своего приложения на использование связки «сервис + BroadcastReceiver» для получения данных из интернета", т.к. он необходим был для сдачи ДЗ, но в дальнейшем в проекте он пока не нужен.

--- app/src/main/java/softing/ubah4ukdev/moviesinfosearcher/ui/detail/DetailFragment.kt
+++ app/src/main/java/softing/ubah4ukdev/moviesinfosearcher/ui/detail/DetailFragment.kt
@@ -1,15 +1,10 @@
 package softing.ubah4ukdev.moviesinfosearcher.ui.detail
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.android.material.snackbar.Snackbar
@@ -21,7 +16,6 @@ import softing.ubah4ukdev.moviesinfosearcher.domain.MoviesRepositoryImpl
 import softing.ubah4ukdev.moviesinfosearcher.ui.extensions.showSnakeBar
 import softing.ubah4ukdev.moviesinfosearcher.ui.extensions.visible
 import softing.ubah4ukdev.moviesinfosearcher.ui.home.HomeFragment
-import softing.ubah4ukdev.moviesinfosearcher.ui.services.MovieService
 import softing.ubah4ukdev.moviesinfosearcher.viewBinding
 
 class DetailFragment : Fragment(R.layout.fragment_detail) {
@@ -35,31 +29,6 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
             ResourceProvider(requireActivity().application),
             MoviesRepositoryImpl
         )
-    }
-
-    private val movieDetailReceiver = object : BroadcastReceiver() {
-        //При получении нашим ресивером данных, передаем их во вьюмодель
-        override fun onReceive(context: Context?, intent: Intent?) {
-            intent?.getParcelableExtra<Movie>(MovieService.ARG_DETAIL)?.let {
-                detailViewModel.loadMovie(it)
-            }
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        //Регистрируем наш movieDetailReceiver
-        IntentFilter(MovieService.ACTION_RESULT).also {
-            LocalBroadcastManager.getInstance(requireContext())
-                .registerReceiver(movieDetailReceiver, it)
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        //Убираем регистрацию нашего movieDetailReceiver
-        LocalBroadcastManager.getInstance(requireContext())
-            .unregisterReceiver(movieDetailReceiver)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -172,16 +141,5 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
         requireArguments().getParcelable<Movie>(HomeFragment.MOVIE_ARG)?.also {
             detailViewModel.loadMovieLocal(it)
         }
-
-        //Запустим сервис для получения детальных данных о фильме
-        requireContext().startService(
-            Intent(
-                requireContext(), MovieService::class.java
-            ).apply {
-                requireArguments().getParcelable<Movie>(HomeFragment.MOVIE_ARG).also {
-                    putExtra(HomeFragment.MOVIE_ARG, it)
-                }
-            }
-        )
     }
 }
