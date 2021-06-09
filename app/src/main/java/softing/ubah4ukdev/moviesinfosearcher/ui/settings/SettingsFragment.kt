5719ac52dbddf4caa61f07553ce9971af517c00d
UBaHbI4 <kinoshnik_ivan@mail.ru>
2021-06-09 16:07:14 +0700

    - Оформление фрагмента настроек. Добавлен SwitchMaterial для сохранения настроки "Разрешить взрослый контент"

--- app/src/main/java/softing/ubah4ukdev/moviesinfosearcher/ui/settings/SettingsFragment.kt
+++ app/src/main/java/softing/ubah4ukdev/moviesinfosearcher/ui/settings/SettingsFragment.kt
@@ -2,11 +2,12 @@ package softing.ubah4ukdev.moviesinfosearcher.ui.settings
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import softing.ubah4ukdev.moviesinfosearcher.R
 import softing.ubah4ukdev.moviesinfosearcher.ResourceProvider
 import softing.ubah4ukdev.moviesinfosearcher.databinding.FragmentSettingsBinding
 import softing.ubah4ukdev.moviesinfosearcher.viewBinding
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
     private val viewBinding: FragmentSettingsBinding by viewBinding(
         FragmentSettingsBinding::bind
